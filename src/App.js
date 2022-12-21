bbac81239bd714603c65dfa37be79b71a9b9b778
vuralevren <evren@altogic.com>
2022-12-21 15:05:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Stats from "./components/stats";
 
 function App() {
+  const [minutesLeft, setMinutesLeft] = useState(
+    5 - (new Date().getMinutes() % 5)
+  );
+
+  useEffect(() => {
+    setInterval(timer, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const timer = () => {
+    setMinutesLeft(5 - (new Date().getMinutes() % 5));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-6 py-6 bg-slate-300 h-full">
+      <h1 className="text-center">
+        Rates are updated instantly every 5 minutes. ({minutesLeft} minutes
+        left)
+      </h1>
+
+      <Stats />
+
+      <ToastContainer
+        position="top-center"
+        autoClose={700}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 }
